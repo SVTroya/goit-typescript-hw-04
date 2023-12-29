d9fbd4609244e090a29af2f64dfd6cd39423c4c4
SVTroya <my.svtroy@gmail.com>
2023-12-29 21:23:58 +0200

    Initial commit

--- src/homework/2.tsx
+++ src/homework/2.tsx
@@ -1,5 +1,16 @@
 import React, {useReducer} from "react";
 
+type State = {
+  isRequestInProgress: boolean,
+  requestStep: 'start' | 'pending' | 'finished' | 'idle'
+}
+
+type Action =
+  | { type: 'START_REQUEST' }
+  | { type: 'PENDING_REQUEST' }
+  | { type: 'FINISH_REQUEST' }
+  | { type: 'RESET_REQUEST' }
+
 const initialState: State = {
   isRequestInProgress: false,
   requestStep: 'idle',
@@ -8,13 +19,13 @@ const initialState: State = {
 function requestReducer(state: State, action: Action): State {
   switch (action.type) {
     case 'START_REQUEST':
-      return { ...state, isRequestInProgress: true, requestStep: 'start' };
+      return {...state, isRequestInProgress: true, requestStep: 'start'};
     case 'PENDING_REQUEST':
-      return { ...state, isRequestInProgress: true, requestStep: 'pending' };
+      return {...state, isRequestInProgress: true, requestStep: 'pending'};
     case 'FINISH_REQUEST':
-      return { ...state, isRequestInProgress: false, requestStep: 'finished' };
+      return {...state, isRequestInProgress: false, requestStep: 'finished'};
     case 'RESET_REQUEST':
-      return { ...state, isRequestInProgress: false, requestStep: 'idle' };
+      return {...state, isRequestInProgress: false, requestStep: 'idle'};
     default:
       return state;
   }
@@ -23,20 +34,20 @@ function requestReducer(state: State, action: Action): State {
 export function RequestComponent() {
   const [requestState, requestDispatch] = useReducer(requestReducer, initialState);
 
-  const startRequest = () => {
-    requestDispatch({ type: 'START_REQUEST' });
+  const startRequest = (): void => {
+    requestDispatch({type: 'START_REQUEST'});
     // Імітуємо запит до сервера
     setTimeout(() => {
-      requestDispatch({ type: 'PENDING_REQUEST' });
+      requestDispatch({type: 'PENDING_REQUEST'});
       // Імітуємо отримання відповіді від сервера
       setTimeout(() => {
-        requestDispatch({ type: 'FINISH_REQUEST' });
+        requestDispatch({type: 'FINISH_REQUEST'});
       }, 2000);
     }, 2000);
   };
 
-  const resetRequest = () => {
-    requestDispatch({ type: 'RESET_REQUEST' });
+  const resetRequest = (): void => {
+    requestDispatch({type: 'RESET_REQUEST'});
   };
 
   return (
